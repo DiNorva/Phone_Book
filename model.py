8745b631fc6547f7f53753eb177ad4757d6f93b3
Diana <diana.norva@yandex.ru>
2022-11-10 16:52:06 +0300

    first commit

---
+++ model.py
@@ -0,0 +1,20 @@
+
+
+path = '/Users/diananorva/Desktop/phone/phone.txt'
+phone_book = []
+
+def getPath():
+    global path
+    return path
+
+def setPath(new_path: str):
+    global path
+    path = new_path
+
+def getPhoneBook() -> list:
+    global phone_book
+    return phone_book
+
+def setPhoneBook(new_phone_book: list):
+    global phone_book
+    phone_book = new_phone_book
